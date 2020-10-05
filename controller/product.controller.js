e6f9c6b485c23cb05a1f51776de6a88ca80cf3b4
Đặng Ngọc Đại <ngocdai2498@gmail.com>
2020-10-05 16:11:31 +0700

    update

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -30,8 +30,8 @@ class APIfeatures {
     paginating() {
         const page = this.queryString.page * 1 || 1;
         const limit = this.queryString.limit * 1 || 4;
-        const skip = (page - 1) * limit;
-        this.query = this.query.skip(skip).limit(limit);
+        const skipt = (page - 1) * limit;
+        this.query = this.query.skip(skipt).limit(limit);
         return this;
     }
 }
