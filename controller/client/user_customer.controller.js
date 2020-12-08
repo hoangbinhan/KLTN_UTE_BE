0abd161341a3434c5d2d8adab9785a4e5f405767
Mac Mini <macmini@Macs-Mac-mini.local>
2020-12-08 18:03:24 +0700

    update

--- controller/client/user_customer.controller.js
+++ controller/client/user_customer.controller.js
@@ -4,18 +4,30 @@ const service = require('../../common/function');
 const bcrypt = require('bcrypt');
 const sendMail = require('.././sendMail.controller');
 const jwt = require('jsonwebtoken');
-const { result } = require('lodash');
-const { findOneAndUpdate } = require('../../models/customers_account.model');
+const {
+    result
+} = require('lodash');
+const {
+    findOneAndUpdate
+} = require('../../models/customers_account.model');
 //
 
-const { CLIENT_URL } = process.env
+const generateSendSseCallback = res => update => {
+    res.write(`data: ${JSON.stringify(update)}\n\n`)
+}
+
+const {
+    CLIENT_URL
+} = process.env
 class APIfeatures {
     constructor(query, queryString) {
         this.query = query;
         this.queryString = queryString;
     }
     filtering() {
-        const queryObj = { ...this.queryString };
+        const queryObj = {
+            ...this.queryString
+        };
         console.log(queryObj);
         const excludedfields = ['page', 'sort', 'limit'];
         excludedfields.forEach(el => delete queryObj[el]);
@@ -45,115 +57,218 @@ const StaffServices = {
     search: async (req, res) => {
         try {
             var regex = new RegExp(req.params.name, 'i');
-            Staff.find({ name: regex }).then((result) => {
+            Staff.find({
+                name: regex
+            }).then((result) => {
                 res.status(200).json(result);
             })
         } catch {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
     register: async (req, res) => {
         try {
-            const { name, email, password, phoneNumber, address  } = req.body
+            const {
+                name,
+                email,
+                password,
+                phoneNumber,
+                address
+            } = req.body
             if (!name || !email || !password)
-                return res.status(400).json({ msg: "Please fill in all fields!" })
+                return res.status(400).json({
+                    msg: "Please fill in all fields!"
+                })
             if (!service.validateEmail(email))
-                return res.status(400).json({ msg: "Invalid emails." })
-            const customer = await CustomerAccount.findOne({ email })
-            if (customer) return res.status(400).json({ msg: "This email already exists." })
+                return res.status(400).json({
+                    msg: "Invalid emails."
+                })
+            const customer = await CustomerAccount.findOne({
+                email
+            })
+            if (customer) return res.status(400).json({
+                msg: "This email already exists."
+            })
             if (password.length < 6)
-                return res.status(400).json({ msg: "Pass word must be at least 6 characters" })
+                return res.status(400).json({
+                    msg: "Pass word must be at least 6 characters"
+                })
             const passwordHash = await bcrypt.hash(password, 12)
             const newStaff = {
-                name, email, password: passwordHash, phoneNumber, address
+                name,
+                email,
+                password: passwordHash,
+                phoneNumber,
+                address
             }
             const post = new CustomerAccount(newStaff)
             await post.save()
             // const activation_token = service.createActivationToken(newStaff);
             // const url = `${CLIENT_URL}/api/staff/activate/${activation_token}`
             // sendMail(email, url, "Verify your email address")
-            res.json({ msg: "Register Success! Please activate your email to start" })
+            res.json({
+                msg: "Register Success! Please activate your email to start"
+            })
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
 
     activateEmail: async (req, res) => {
         try {
-            const { activation_token } = req.body
+            const {
+                activation_token
+            } = req.body
             const staff = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN_SECRET)
 
-            const { staffID, name, email, password } = staff
+            const {
+                staffID,
+                name,
+                email,
+                password
+            } = staff
 
-            const check = await Staff.findOne({ email })
-            if (check) return res.status(400).json({ msg: "This email already exists." })
+            const check = await Staff.findOne({
+                email
+            })
+            if (check) return res.status(400).json({
+                msg: "This email already exists."
+            })
 
             const newStaff = new Staff({
-                staffID, name, email, password
+                staffID,
+                name,
+                email,
+                password
             })
 
             await newStaff.save()
 
-            res.json({ msg: "Account has been activated!" })
+            res.json({
+                msg: "Account has been activated!"
+            })
 
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
 
-    addToCart: async (req,res) =>{
-        try{
+    addToCart: async (req, res) => {
+        try {
             let isAdd = false
-            const {email, product, quantity} = req.body
-            const productInStore = await Product.findOne({_id:product})
-            if(!productInStore) return res.status(500).json({msg: 'product invalid'})
-            if((productInStore.quantity - quantity) < 0) return res.status(500).json({msg: 'quantity is too large'})
-            
-            const customer =  await CustomerAccount.findOne({email})
-            if(!customer) return res.status(500).json({msg: 'customer invalid'})
-            for(let i = 0; i<customer.cart.length;i++){
-                if(customer.cart[i].id === product){
+            const {
+                email,
+                product,
+                quantity
+            } = req.body
+            const productInStore = await Product.findOne({
+                _id: product
+            })
+            if (!productInStore) return res.status(500).json({
+                msg: 'product invalid'
+            })
+            if ((productInStore.quantity - quantity) < 0) return res.status(500).json({
+                msg: 'quantity is too large'
+            })
+
+            const customer = await CustomerAccount.findOne({
+                email
+            })
+            if (!customer) return res.status(500).json({
+                msg: 'customer invalid'
+            })
+            for (let i = 0; i < customer.cart.length; i++) {
+                if (customer.cart[i].id === product) {
                     customer.cart[i].quantity += quantity
                     isAdd = true
                     break
                 }
             }
-            if(!isAdd){
-                customer.cart.push({id: product, quantity:quantity})
+            if (!isAdd) {
+                customer.cart.push({
+                    id: product,
+                    quantity: quantity
+                })
             }
-            await CustomerAccount.findOneAndUpdate({email}, customer)
+            await CustomerAccount.findOneAndUpdate({
+                email
+            }, customer)
             // if(isAdd){
             //     await CustomerAccount.findOneAndUpdate({email}, {$set: {cart: {id: product, quantity: isOldQuantity+ quantity}}})
             // }else{
             //     await CustomerAccount.findOneAndUpdate({email}, {$push: {cart: {id: product, quantity: quantity}}})
             // }
-          
-            return res.status(200).json({msg: 'successful'})
-        }catch(err){
-            return res.status(500).json({msg: err.message})
+
+            return res.status(200).json({
+                msg: 'successful'
+            })
+        } catch (err) {
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
 
-    getCart: async(req,res)=>{
-        try{
-          CustomerAccount.watch().on('change', data=>console.log(data))
-           
-        }catch(err){
-            return res.status(500).json({msg: err.message})
+    getCart: async (req, res) => {
+        const {email} = req
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        })
+        res.flushHeaders();
+        try {
+            CustomerAccount.watch([
+                {
+                    '$match': {
+                        'email': email,
+                    },
+                }
+            ]).on('change', (data) =>{
+                const sseFormattedResponse = `data: ${data.fullDocument.cart}\n\n`;
+                res.write(sseFormattedResponse);
+            })
+        } catch (err) {
+            return res.status(500).json({
+                msg: err.message
+            })
         }
-    }
-    
-    ,
-    
+    },
+
     login: async (req, res) => {
         try {
-            const { email, password } = req.body
-            const customer = await CustomerAccount.findOne({ email })
-            if (!customer) return res.status(400).json({ msg: "This email does not exist." })
+            const {
+                email,
+                password
+            } = req.body
+            const customer = await CustomerAccount.findOne({
+                email
+            })
+            if (!customer) return res.status(400).json({
+                msg: "This email does not exist."
+            })
             const isMatch = await bcrypt.compare(password, customer.password)
-            if (!isMatch) return res.status(400).json({ msg: "Password is incorrect." })
-            const refresh_token = service.createRefreshToken({ email: customer.email, name: customer.name, role: customer.role })
-            const token = service.createAccessToken({ email: customer.email, name: customer.name, role: customer.role })
+            if (!isMatch) return res.status(400).json({
+                msg: "Password is incorrect."
+            })
+            const refresh_token = service.createRefreshToken({
+                email: customer.email,
+                name: customer.name,
+                role: customer.role
+            })
+            const token = service.createAccessToken({
+                email: customer.email,
+                name: customer.name,
+                role: customer.role
+            })
             res.cookie('refreshtoken', refresh_token, {
                 httpOnly: true,
                 path: '/api/staff/refresh_token',
@@ -167,70 +282,104 @@ const StaffServices = {
                 }
             })
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
 
     getAccessToken: (req, res) => {
         try {
             const rf_token = req.cookies.refreshtoken
-            if (!rf_token) return res.status(400).json({ msg: "Please login now!" })
+            if (!rf_token) return res.status(400).json({
+                msg: "Please login now!"
+            })
 
             jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, staff) => {
-                if (err) return res.status(400).json({ msg: "Please login now!" })
+                if (err) return res.status(400).json({
+                    msg: "Please login now!"
+                })
 
-                const access_token = service.createAccessToken({ id: staff.id })
+                const access_token = service.createAccessToken({
+                    id: staff.id
+                })
                 res.status(200).json({
                     status: "gettoken success",
                     refresh_token: access_token
                 })
             })
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
     forgotPassword: async (req, res) => {
         try {
-            const { email } = req.body
-            const staff = await Staff.findOne({ email })
-            if (!staff) return res.status(400).json({ msg: "This email does not exist." })
+            const {
+                email
+            } = req.body
+            const staff = await Staff.findOne({
+                email
+            })
+            if (!staff) return res.status(400).json({
+                msg: "This email does not exist."
+            })
 
-            const access_token = service.createAccessToken({ id: staff.staffID })
+            const access_token = service.createAccessToken({
+                id: staff.staffID
+            })
             const url = `${CLIENT_URL}/api/staff/reset/${access_token}`
 
             sendMail(email, url, "Reset your password")
-            res.json({ msg: "Re-send the password, please check your email." })
+            res.json({
+                msg: "Re-send the password, please check your email."
+            })
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
     resetPassword: async (req, res) => {
         try {
-            const { password } = req.body
+            const {
+                password
+            } = req.body
             // console.log(password)
             const passwordHash = await bcrypt.hash(password, 12)
             // console.log(req.user)
-            await Staff.findOneAndUpdate({ staffID: req.staff.id }, {
+            await Staff.findOneAndUpdate({
+                staffID: req.staff.id
+            }, {
                 password: passwordHash
             })
-            res.json({ msg: "Password successfully changed!" })
+            res.json({
+                msg: "Password successfully changed!"
+            })
 
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
     getStaffInfor: async (req, res) => {
         try {
-            const feature = new APIfeatures(Staff.findOne({ staffID: req.staff.id }).select('-password'), req.query)
-            .filtering()
-            .sorting()
+            const feature = new APIfeatures(Staff.findOne({
+                    staffID: req.staff.id
+                }).select('-password'), req.query)
+                .filtering()
+                .sorting()
             // const staff = await Staff.findOne({ staffID: req.staff.id }).select('-password')
             const staff = await feature.query
 
             // console.log(user);
             res.json(staff)
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
     getStaffsAllInfor: async (req, res) => {
@@ -239,7 +388,9 @@ const StaffServices = {
             const staffs = await Staff.find().select('-password')
             res.json(staffs)
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
     // avatar  
@@ -250,10 +401,16 @@ const StaffServices = {
             //     name, // avatar
             // })
             // res.json({ msg: "Update Success!" })
-            const { staffID } = req.body
+            const {
+                staffID
+            } = req.body
             const updateField = service.genUpdate(req.body,
                 ['name', 'status'])
-            await Staff.findOneAndUpdate({ staffID }, updateField, { new: true }, (err, result) => {
+            await Staff.findOneAndUpdate({
+                staffID
+            }, updateField, {
+                new: true
+            }, (err, result) => {
                 if (result || !err) {
                     res.json(result)
                 } else {
@@ -261,20 +418,30 @@ const StaffServices = {
                 }
             })
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
     updateStaffsRole: async (req, res) => {
         try {
-            const { role } = req.body
+            const {
+                role
+            } = req.body
 
-            await Staff.findOneAndUpdate({ staffID: req.params.id }, {
+            await Staff.findOneAndUpdate({
+                staffID: req.params.id
+            }, {
                 role
             })
 
-            res.json({ msg: "Update Success!" })
+            res.json({
+                msg: "Update Success!"
+            })
         } catch (err) {
-            return res.status(500).json({ msg: err.message })
+            return res.status(500).json({
+                msg: err.message
+            })
         }
     },
     deleteStaff: async (req, res) => {
@@ -288,8 +455,12 @@ const StaffServices = {
             //     res.json(false)
             // }
 
-            const { staffID } = req.body
-            await Staff.deleteOne({ staffID }, async (err, result) => {
+            const {
+                staffID
+            } = req.body
+            await Staff.deleteOne({
+                staffID
+            }, async (err, result) => {
                 if (result || !err) {
                     res.json(result)
                 } else {
@@ -302,8 +473,16 @@ const StaffServices = {
     },
     deleteStaffStatus: async (req, res) => {
         try {
-            const { staffID } = req.body
-            await Staff.findOneAndUpdate({ staffID }, { status: 'INACTIVE' }, { new: true }, async (err, result) => {
+            const {
+                staffID
+            } = req.body
+            await Staff.findOneAndUpdate({
+                staffID
+            }, {
+                status: 'INACTIVE'
+            }, {
+                new: true
+            }, async (err, result) => {
                 if (result || !err) {
                     console.log(result)
                     res.json(result)
